f95593d6b25761a1f6d98c17ee88934a985fbf77
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-07-17 02:51:05 +0700

    Khôi phục healthcheck trong khi tách đồng bộ Meta nền

--- start.js
+++ start.js
@@ -17,11 +17,9 @@ try {
 
 await import("./patch-v7-pancake-classifier.js");
 await import("./materialize-v7-dashboard.js");
-await import("./patch-v7-meta-primary.js");
 await import("./patch-v7-report-accuracy.js");
 await import("./patch-v7-product-detection.js");
 await import("./patch-v7-navigation.js");
-await import("./patch-v7-pancake-toggle.js");
 await import("./patch-v7-lead-filters.js");
 await import("./patch-learning-client.js");
 await import("./patch-server.js");
