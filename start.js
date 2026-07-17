12409ea2c59c5771cd2b3861a9018988393b8498
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-07-18 01:39:42 +0700

    Activate restored V7 Google Drive connection

--- start.js
+++ start.js
@@ -40,5 +40,6 @@ await import("./patch-v7-runtime-integrity.js");
 await import("./patch-learning-client.js");
 await import("./patch-drive-slide-manager-v2.js");
 await import("./patch-drive-login-only.js");
+await import("./patch-drive-v7-mode.js");
 await import("./patch-server.js");
 await import("./server-fixed.js");
