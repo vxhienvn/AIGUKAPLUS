b8e21f642fbb09951c457f41d4c945e2d0c949c2
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-07-18 14:38:55 +0700

    Remove invalid pages_read_user_content OAuth scope

--- patch-meta-pages-messaging-scope.js
+++ patch-meta-pages-messaging-scope.js
@@ -2,14 +2,17 @@ import fs from "node:fs";
 
 const file = "meta-facebook-login.js";
 let source = fs.readFileSync(file, "utf8");
-const oldScopes = '"ads_read,business_management,pages_show_list,pages_read_engagement"';
-const newScopes = '"ads_read,business_management,pages_show_list,pages_read_engagement,pages_messaging,pages_manage_metadata,pages_read_user_content"';
-if (source.includes(oldScopes)) {
-  source = source.replace(oldScopes, newScopes);
-  fs.writeFileSync(file, source, "utf8");
-  console.log("[AIGUKA] Meta OAuth now requests pages_messaging");
-} else if (source.includes("pages_messaging")) {
-  console.log("[AIGUKA] Meta OAuth pages_messaging scope already present");
-} else {
-  throw new Error("META_PAGES_MESSAGING_SCOPE_ANCHOR_NOT_FOUND");
-}
+const baseScopes = '"ads_read,business_management,pages_show_list,pages_read_engagement"';
+const invalidScopes = '"ads_read,business_management,pages_show_list,pages_read_engagement,pages_messaging,pages_manage_metadata,pages_read_user_content"';
+const validScopes = '"ads_read,business_management,pages_show_list,pages_read_engagement,pages_messaging,pages_manage_metadata"';
+
+if (source.includes(invalidScopes)) source = source.replace(invalidScopes, validScopes);
+else if (source.includes(baseScopes)) source = source.replace(baseScopes, validScopes);
+else if (!source.includes("pages_messaging")) throw new Error("META_PAGES_MESSAGING_SCOPE_ANCHOR_NOT_FOUND");
+
+const filterNeedle = ".filter(Boolean);";
+const filterReplacement = '.filter((value) => value && value !== "pages_read_user_content");';
+if (source.includes(filterNeedle)) source = source.replace(filterNeedle, filterReplacement);
+
+fs.writeFileSync(file, source, "utf8");
+console.log("[AIGUKA] Meta OAuth requests only valid Page and Messenger scopes");
